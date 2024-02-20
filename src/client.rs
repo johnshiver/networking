081a25fb72880b1690fba30c0450311aca91f494
John Shiver <john@johnshiver.org>
2024-02-19 22:01:06 -0500

    add intercept example

--- src/client.rs
+++ src/client.rs
@@ -2,22 +2,53 @@ pub mod api {
     tonic::include_proto!("ping");
 }
 
+use rand::Rng;
+use tonic::service::Interceptor;
+use tonic::{Request, Status};
+use tonic::transport::Endpoint;
 use crate::api::ping_service_client::PingServiceClient;
 use crate::api::PingRequest;
 
+
+
+
+
+struct RandomFailInterceptor;
+
+impl Interceptor for RandomFailInterceptor {
+    fn call(&mut self, request: Request<()>) -> Result<Request<()>, Status> {
+        if rand::thread_rng().gen_bool(0.5) {
+            println!("Allowing request to proceed.");
+            Ok(request)
+        } else {
+            println!("Blocking request with an error.");
+            Err(Status::internal("Randomly blocked by interceptor"))
+        }
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut client = PingServiceClient::connect("http://[::1]:50051").await?;
+    // Create the interceptor
+    let interceptor = RandomFailInterceptor;
 
+    // Create a channel and attach the interceptor
+    let channel = Endpoint::from_static("http://[::1]:50051")
+        .connect()
+        .await?;
+
+    // Create the client with the intercepted channel
+    let mut client = PingServiceClient::with_interceptor(channel,interceptor);
+
+    // Example request
     let request = tonic::Request::new(PingRequest {
         message: "hello there".to_string(),
     });
 
-    println!("Sending request to gRPC Server...");
-    let response = client.ping(request).await?;
-    let res = response.get_ref();
-    println!("RESPONSE={:?}", response);
-    println!("RESPONSE={:?}", res);
+    match client.ping(request).await {
+        Ok(response) => println!("RESPONSE={:?}", response.get_ref()),
+        Err(e) => println!("Request failed: {:?}", e),
+    }
 
     Ok(())
 }
